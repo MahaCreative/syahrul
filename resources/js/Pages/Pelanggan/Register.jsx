d305010466b4e75a3f5fba25704456f06e1c83ce
MahaCreative <gunturmadjid.3@gmail.com>
2024-06-22 06:49:29 -0700

    update

--- resources/js/Pages/Pelanggan/Register.jsx
+++ resources/js/Pages/Pelanggan/Register.jsx
@@ -170,7 +170,7 @@ export default function Register() {
                         required
                         title={"Konfirmasi Password"}
                         name={"password_confirmation"}
-                        type="password"
+                        type="passwoard"
                         errors={errors.password_confirmation}
                         onChange={(e) =>
                             setData({
